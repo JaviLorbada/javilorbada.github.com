ce4c7776b103f6e587b06bb6ea76fdf9ccac6e50
Javi Lorbada <javugi@gmail.com>
2013-03-22 00:52:44 +0100

    Social icons have been added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+
+// Social Icons Animation
+	
+		/* Variables */
+		
+			var social = jQuery("#social li");
+			
+		/* CSS */
+		
+			social.css({opacity: .5});
+		
+		/* Hover */
+		
+			social.hover(
+			    function()
+			    {
+			        jQuery(this).stop(true,true).animate({opacity: 1}, 200);
+			    },
+			    function()
+			    {
+			        jQuery(this).stop(true,true).animate({opacity: .5}, 200);
+			    }
+			);
